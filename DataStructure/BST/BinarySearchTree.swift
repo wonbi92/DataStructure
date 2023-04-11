5f48816252e8dec06a3676341112daac43824739
wonbi92 <bin9239@gmail.com>
2023-04-12 06:37:42 +0900

    feat: BST나머지 구현

--- DataStructure/BST/BinarySearchTree.swift
+++ DataStructure/BST/BinarySearchTree.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class BinarySearchTree<T: Comparable> {
+final class BinarySearchTree1<T: Comparable> {
     private var root: Node<T>?
     
     var isEmpty: Bool {
@@ -15,7 +15,7 @@ final class BinarySearchTree<T: Comparable> {
     }
     
     var asString: String {
-        guard let root else { return "" }
+        guard let root else { return "Tree is Empty" }
         
         return treeString(root) { ("\($0.data)", $0.left, $0.right) }
     }
@@ -68,6 +68,128 @@ final class BinarySearchTree<T: Comparable> {
     }
     
     func remove(_ data: T) {
+        guard let node = search(data) else { return }
         
+        if node.isRoot {
+            removeRoot(node: node)
+        } else if node.isLeaf {
+            removeLeaf(node: node)
+        } else if !node.isFull {
+            removeInternal(node: node)
+        } else {
+            removeFull(node: node)
+        }
+    }
+    
+    private func removeRoot(node: Node<T>) {
+        // root가 LeafNode일 경우
+        if node.isLeaf {
+            root = nil
+        // root가 왼쪽 노드만 가지고 있을 경우
+        } else if node.hasOnlyLeft {
+            var lastRightNode = node.left
+            
+            // 루트 왼쪽노드의 가장 오른쪽에 있는 노드를 찾는 로직
+            while let nextNode = lastRightNode?.right {
+                lastRightNode = nextNode
+            }
+            
+            // 가장 오른쪽에 있는 노드가 왼쪽에 데이터를 가지고 있을 경우
+            if let leftNode = lastRightNode?.left {
+                lastRightNode?.left?.parent = lastRightNode?.parent
+                lastRightNode?.parent?.right = leftNode
+            // 가장 오른쪽에 있는 노드가 LeafNode일 경우
+            } else {
+                lastRightNode?.parent?.right = nil
+            }
+            
+            // 노드 교체 로직
+            root?.left?.parent = lastRightNode
+            lastRightNode?.left = root?.left
+            root = lastRightNode
+            
+            lastRightNode?.parent = nil
+        // root가 FullNode이거나 오른쪽 노드만 가지고 있는 경우
+        } else {
+            var lastLeftNode = node.right
+            
+            // 루트 오른쪽노드의 가장 왼쪽에 있는 노드를 찾는 로직
+            while let nextNode = lastLeftNode?.left {
+                lastLeftNode = nextNode
+            }
+            
+            // 가장 왼쪽에 있는 노드가 오른쪽에 데이터를 가지고 있을 경우
+            if let rightNode = lastLeftNode?.right {
+                lastLeftNode?.right?.parent = lastLeftNode?.parent
+                lastLeftNode?.parent?.left = rightNode
+            // 가장 왼쪽에 있는 노드가 LeafNode일 경우
+            } else {
+                lastLeftNode?.parent?.left = nil
+            }
+            
+            // 노드 교체 로직
+            if node.left != nil {
+                root?.left?.parent = lastLeftNode
+                lastLeftNode?.left = root?.left
+            }
+            root?.right?.parent = lastLeftNode
+            lastLeftNode?.right = root?.right
+            root = lastLeftNode
+            
+            lastLeftNode?.parent = nil
+        }
+    }
+    
+    private func removeLeaf(node: Node<T>) {
+        if node.data == node.parent?.left?.data {
+            node.parent?.left = nil
+        } else {
+            node.parent?.right = nil
+        }
+    }
+    
+    private func removeInternal(node: Node<T>) {
+        guard let parentNode = node.parent else { return }
+        
+        if node.hasOnlyLeft {
+            if node.data < parentNode.data {
+                parentNode.left = node.left
+            } else {
+                parentNode.right = node.left
+            }
+        } else {
+            if node.data < parentNode.data {
+                parentNode.left = node.right
+            } else {
+                parentNode.right = node.right
+            }
+        }
+    }
+    
+    private func removeFull(node: Node<T>) {
+        var lastLeftNode = node.right
+        
+        // node 오른쪽노드의 가장 왼쪽에 있는 노드를 찾는 로직
+        while let nextNode = lastLeftNode?.left {
+            lastLeftNode = nextNode
+        }
+        
+        // 가장 왼쪽에 있는 노드가 오른쪽에 데이터를 가지고 있을 경우
+        if let rightNode = lastLeftNode?.right {
+            lastLeftNode?.right?.parent = lastLeftNode?.parent
+            lastLeftNode?.parent?.left = rightNode
+        // 가장 왼쪽에 있는 노드가 LeafNode일 경우
+        } else {
+            lastLeftNode?.parent?.left = nil
+        }
+        
+        // 노드 교체 로직
+        node.right?.parent = lastLeftNode
+        node.left?.parent = lastLeftNode
+        lastLeftNode?.right = node.right
+        lastLeftNode?.left = node.left
+        
+        node.parent?.left = lastLeftNode
+        lastLeftNode?.parent = node.parent
     }
 }
--- DataStructure/BST/Node.swift
+++ DataStructure/BST/Node.swift
@@ -13,7 +13,52 @@ final class Node<T: Comparable> {
     var left: Node<T>?
     var right: Node<T>?
     
+    var isRoot: Bool {
+        return parent == nil
+    }
+    
+    var isLeaf: Bool {
+        return left == nil && right == nil
+    }
+    
+    var isFull: Bool {
+        return left != nil && right != nil
+    }
+    
+    var hasOnlyLeft: Bool {
+        return left != nil && right == nil
+    }
+    
     init(data: T) {
         self.data = data
+    }
+}
+
+struct Traverser {
+    static func traversePreOrder<T: Comparable>(node: Node<T>?) {
+        guard let node = node else {
+            return
+        }
+        print(node.data)
+        traversePreOrder(node: node.left)
+        traversePreOrder(node: node.right)
+    }
+    
+    static func traverseInOrder<T: Comparable>(node: Node<T>?) {
+        guard let node = node else {
+            return
+        }
+        traversePreOrder(node: node.left)
+        print(node.data)
+        traversePreOrder(node: node.right)
+    }
+    
+    static func traversePostOrder<T: Comparable>(node: Node<T>?) {
+        guard let node = node else {
+            return
+        }
+        traversePreOrder(node: node.left)
+        traversePreOrder(node: node.right)
+        print(node.data)
     }
 }
